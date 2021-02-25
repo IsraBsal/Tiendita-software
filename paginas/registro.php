db297f160273dce74b99904ca373a83c61dae78c
israbsal <desintegrado19@gmail.com>
2021-02-25 10:13:16 -0600

    Validando que no acepta campos vacios

--- paginas/registro.php
+++ paginas/registro.php
@@ -2,6 +2,7 @@ Inicia sesion para poder visualizar y comprar los productos
 <?php
 
 	if(isset($enviarregistro)){
+		
 		$correo=clear($correo);
 		$password=clear($password);
 		$nombre=clear($nombre);
@@ -11,11 +12,17 @@ Inicia sesion para poder visualizar y comprar los productos
 		$estado=clear($estado);
 		$codpostal=clear($codpostal);
 		$telefono=clear($telefono);
-	
-		$ssql="INSERT INTO usuarios (Correo_Electronico,Password,Nombre,Apellidos,Direccion,Ciudad,Estado,Codpostal,Telefono) VALUES ('$correo','$password','$nombre','$apellidos','$direccion','$ciudad','$estado','$codpostal','$telefono')";
-		$query=mysqli_query($enlace,$ssql);
 		
-		alert("Registro exitoso",1,'login');
+		
+		if(!empty($correo) && !empty($password) && !empty($nombre) && !empty($apellidos) && !empty($direccion) && !empty($ciudad) && !empty($estado) && !empty($codpostal) && !empty($telefono)){
+			$ssql="INSERT INTO usuarios (Correo_Electronico,Password,Nombre,Apellidos,Direccion,Ciudad,Estado,Codpostal,Telefono) VALUES ('$correo','$password','$nombre','$apellidos','$direccion','$ciudad','$estado','$codpostal','$telefono')";
+			$query=mysqli_query($enlace,$ssql);
+			alert("Registro exitoso",1,'login');
+		}
+		else{
+			alert("Error, campo vacio",0,'registro');
+		}
+		
 	
 	}
 
